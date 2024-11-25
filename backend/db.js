4b6f921730f5c533fc50c52cec1c20bd63bcbe84
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-11-25 21:01:30 +0530

    till video 44

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,17 @@
+// Connecting to database 
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://localhost:27017/"; // connection
+
+
+
+// Mongoose manages the database for us
+
+// older Version (this won't work)
+// const connectToMongo = () => {
+//     mongoose.connect(mongoURI, () => {
+//       console.log("Connected to MongoDB successfully");
+//     }); // this function is wrong as it doesn't take a callback as argument
+// };
 
 const connectToMongo = async () => {
   try {
@@ -13,5 +25,7 @@ const connectToMongo = async () => {
     console.error("Failed to connect to MongoDB", err);
   }
 };
+
+
 
 module.exports = connectToMongo;
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,39 @@
+// entry point of the application
+// this index.js will be an express server
+
 const connectToMongo = require('./db');
-const express = require('express');
+const express = require('express');  // requiring express 
+// express is a web framework for nodejs which has all the tools which makes our backend development easier.
 connectToMongo();
 
 const app = express();
-const port = 3000
+const port = 3000;
+
+
+// // Middleware
+// app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello Rudresh!');
-});
+  res.send('Hello Rudresh!'); 
+  // res.send('<h1>Hey<h1>'); 
+  // res.send('<h1>Hey<h1>'); 
+}); 
+
+// But we won't make our routes like this because this way the code will get messy
+// app.get('/api/v1/login', (req, res) => {
+//   res.send('Hello login!'); 
+// }); 
+
+// app.get('/api/v1/signup', (req, res) => {
+//   res.send('Hello signup!'); 
+// }); 
+
+
+// // More Available routes:
+app.use('/api/auth', require('./routes/auth')); // making route
+app.use('/api/notes', require('./routes/notes')); // making route
+
+
 
 app.listen(port, ()=> {
   console.log(`Example app listening at http://localhost:${port}`);
